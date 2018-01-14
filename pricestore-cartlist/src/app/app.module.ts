938607b892a489c43b8e88a5c073411656509ed9
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-01-14 17:47:56 -0200

    Esqueleto de lista de compras

--- pricestore-cartlist/src/app/app.module.ts
+++ pricestore-cartlist/src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { ProductsCartlistModule } from './products-cartlist/products-cartlist.module';
-import { CartlistListComponent } from './cartlist-list/cartlist-list.component';
+import { HomeComponent } from './home/home.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartlistListComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    ProductsCartlistModule
+    ProductsCartlistModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pricestore-cartlist/src/app/products-cartlist/products-cartlist.module.ts
+++ pricestore-cartlist/src/app/products-cartlist/products-cartlist.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { CartlistListComponent } from './cartlist-list/cartlist-list.component';
+
+const cartlistRoutes = [
+  {path: 'cartlists', component: CartlistListComponent}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(
+      cartlistRoutes // <-- debugging purposes only
+    )
   ],
   declarations: [CartlistListComponent]
 })

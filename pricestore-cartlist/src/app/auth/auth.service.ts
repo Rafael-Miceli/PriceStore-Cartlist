b336d1fccfc90b91c4444b570b14ee6cb284d64a
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-01-17 07:25:04 -0200

    Verificando se usuario esta logado com CanActivate

---
+++ pricestore-cartlist/src/app/auth/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router/src/interfaces';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular//common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService implements CanActivate {
+
+  userLoggerdIn = false;
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    const url = 'http://localhost:5000/api/Auth';
+
+    return this.verifyLogin(url).map(
+      r => {
+        console.log(r);
+        return false;
+      }
+    );
+  }
+
+  verifyLogin(url: string): Observable<boolean> {
+
+    return this.http.post(url, `{"name": "rafael", "password": "123"}`).map(r => {console.log(r); return false; });
+
+    // if (this.userLoggerdIn) {
+    //   console.log('Usuario logado');
+    //   return true;
+    // }
+
+    // console.log('Usuario Nao logado');
+
+    // return false;
+  }
+
+}
--- pricestore-cartlist/src/app/products-cartlist/products-cartlist.module.ts
+++ pricestore-cartlist/src/app/products-cartlist/products-cartlist.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CartlistListComponent } from './cartlist-list/cartlist-list.component';
+import { AuthService } from '../auth/auth.service';
 
 const cartlistRoutes = [
-  {path: 'cartlists', component: CartlistListComponent}
+  {path: 'cartlists', component: CartlistListComponent, canActivate: [AuthService]}
 ];
 
 @NgModule({
@@ -13,8 +15,10 @@ const cartlistRoutes = [
     CommonModule,
     RouterModule.forChild(
       cartlistRoutes // <-- debugging purposes only
-    )
+    ),
+    HttpClientModule
   ],
-  declarations: [CartlistListComponent]
+  declarations: [CartlistListComponent],
+  providers: [AuthService]
 })
 export class ProductsCartlistModule { }

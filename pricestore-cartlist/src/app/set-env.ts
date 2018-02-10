7abd1b11cc01069975de5de003ee68830af77361
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-02-10 18:33:42 -0200

    Adicionado set-env para criar arquivo com comando a partir das variaveis de ambiente

---
+++ pricestore-cartlist/src/app/set-env.ts
@@ -0,0 +1,30 @@
+import { writeFile } from 'fs';
+import { argv } from 'yargs';
+
+// This is good for local dev environments, when it's better to
+// store a projects environment variables in a .gitignore'd file
+require('dotenv').config();
+
+// Would be passed to script like this:
+// `ts-node set-env.ts --environment=dev`
+// we get it from yargs's argv object
+const environment = process.env.ENVNAME;
+const isProd = environment === 'prod';
+
+const targetPath = `./src/environments/environment.ts`;
+const envConfigFile = `
+export const environment = {
+  production: ${isProd},
+  apiEndpoint: "${process.env.APIENDPOINT}",
+  envName: "${process.env.ENVNAME}",
+  badActorVersion: 1
+};
+`;
+
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Output generated at ${targetPath}`);
+});

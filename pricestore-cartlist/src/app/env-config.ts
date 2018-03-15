6fc3a2698c88bcdc8d887d7f5cb1231acac95601
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-15 13:25:28 -0300

    Atualizado env-config

--- pricestore-cartlist/src/app/env-config.ts
+++ pricestore-cartlist/src/app/env-config.ts
@@ -5,24 +5,42 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class EnvConfig {
 
-    public config: any;
+    private config: Object = null;
+    private env: Object = null;
 
     constructor(private http: Http) {
-        this.http.get('assets/env-config.json')
-        .map(res => res.json())
-        .toPromise()
-        .then((config) => {
-            // do stuff with the config
-            console.log('Em EnvConfig ');
-            console.log(config);
 
-            this.config = config;
-        });
     }
 
-    public getConfig(): Promise<any> {
-        return this.http.get('assets/env-config.json')
-        .map(res => res.json())
-        .toPromise();
+    /**
+     * Use to get the data found in the second file (config file)
+     */
+    public getConfig(key: any) {
+        return this.config[key];
+    }
+
+    /**
+     * Use to get the data found in the first file (env file)
+     */
+    public getEnv(key: any) {
+        return this.env[key];
+    }
+
+
+    /**
+ * This method:
+ *   a) Loads "env.json" to get the current working environment (e.g.: 'production', 'development')
+ *   b) Loads "config.[env].json" to get all env's variables (e.g.: 'config.development.json')
+ */
+    public async load() {
+        await this.http.get('assets/env-config.json').map(res => res.json()).catch((error: any): any => {
+            console.log('Configuration file "assets/env-config.json" could not be read');
+
+        }).toPromise().then(envResponse => {
+
+            this.config = this.env = envResponse;
+        });
+
+        return this.config;
     }
 }

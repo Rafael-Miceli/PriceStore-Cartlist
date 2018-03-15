1c564c64dec0e3fe01b533ed60b6bd0841ec72af
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-03-15 13:33:19 -0300

    main.ts pegando valor de ambiente de novo config

--- pricestore-cartlist/src/main.ts
+++ pricestore-cartlist/src/main.ts
@@ -1,12 +1,24 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, OnInit } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
+import { EnvConfig } from './app/env-config';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+export class Main implements OnInit {
+
+  private production: string;
+
+  constructor(private envConfig: EnvConfig) { }
+
+  ngOnInit() {
+    const environment: string = this.envConfig.getConfig('production');
+
+    this.production = environment;
+  }
+
+  if (production) {
+    enableProdMode();
+  }
